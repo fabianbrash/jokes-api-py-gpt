b317f14663e1267cbce6a29040050f9cb2f4102f
frjb <frjb@frb79hotmail.onmicrosoft.com>
2024-02-09 15:14:27 -0500

    implemented azure key vault

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask_restful import Api, Resource
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token
 from azure.cosmos import CosmosClient
 from flasgger import Swagger, swag_from
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
 import uuid
 import configparser
 
@@ -11,22 +13,25 @@ config = configparser.ConfigParser()
 config.read('config.ini')
 
 # Get CosmosDB credentials from the config file
-COSMOSDB_ENDPOINT = config.get('CosmosDB', 'endpoint')
-COSMOSDB_KEY = config.get('CosmosDB', 'key')
+#COSMOSDB_ENDPOINT = config.get('CosmosDB', 'endpoint')
+#COSMOSDB_KEY = config.get('CosmosDB', 'key')
 DATABASE_ID = config.get('CosmosDB', 'database_id')
 CONTAINER_ID = config.get('CosmosDB', 'container_id')
 USER_ID = config.get('jwt', 'user')
 PWD = config.get('jwt', 'password')
 JWT_SECRET_ID = config.get('jwt', 'jwt_secret_id')
 
+# Azure Key Vault Configuration
+KEY_VAULT_URL = "https://wus-app-kv.vault.azure.net/"
+#KEY_VAULT_CLIENT_ID = JOKE_API_CLIENT_ID
 
 app = Flask(__name__)
 api = Api(app)
 app.config['JWT_SECRET_KEY'] = JWT_SECRET_ID  # Change this to a secure secret key
+
 # Set the expiration time for access tokens (in seconds)
 # the default is 15 minutes or 900 seconds
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = 1800  # 30 minutes
-
 jwt = JWTManager(app)
 
 # Swagger configuration
@@ -37,14 +42,41 @@ app.config['SWAGGER'] = {
 
 swagger = Swagger(app)
 
+# Use DefaultAzureCredential
+# We are using a system managed account here
+credential = DefaultAzureCredential()
+
+# Use DefaultAzureCredential with explicit client ID and client secret
+# use the below if you are using a user created managed account
+#credential = DefaultAzureCredential(
+#    client_id=KEY_VAULT_CLIENT_ID,
+#    client_secret=JOKE_API_CLIENT_SECRET
+#)
+
+# Create a Key Vault SecretClient
+secret_client = SecretClient(vault_url=KEY_VAULT_URL, credential=credential)
+
+# Get secrets from Key Vault
+cosmosdb_endpoint = secret_client.get_secret("wus-cosmo-endpoint").value
+cosmosdb_key = secret_client.get_secret("wus-cosmo-write-key").value
+
+
 # Initialize Flask app and CosmosDB client
 #app = Flask(__name__)
 #api = Api(app)
 
 # Create CosmosDB client
-cosmos_client = CosmosClient(COSMOSDB_ENDPOINT, COSMOSDB_KEY)
+#cosmos_client = CosmosClient(COSMOSDB_ENDPOINT, COSMOSDB_KEY)
+#database = cosmos_client.get_database_client(DATABASE_ID)
+#container = database.get_container_client(CONTAINER_ID)
+
+# Create CosmosDB client
+cosmos_client = CosmosClient(cosmosdb_endpoint, cosmosdb_key)
 database = cosmos_client.get_database_client(DATABASE_ID)
 container = database.get_container_client(CONTAINER_ID)
+
+#Swagger Docs are here
+# http(s)://{URL_OR_IP}:{PORT(OPTIONAL)}/apidocs/
 
 # API Resource for jokes
 class JokeResource(Resource):
